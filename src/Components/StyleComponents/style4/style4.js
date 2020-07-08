29a49823a261dd6151708eabc70f123717858538
unknown <boureimatapily1@gmail.com>
2020-07-08 03:47:36 +0000

    MOD create style 4 content

---
+++ src/Components/StyleComponents/style4/style4.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./style4.css"
+
+export default function style4() {
+    return (
+        <div className="mainStyle4">
+            <div class="threeD">
+            Make your dream come true
+            </div>
+        </div>
+    )
+}
