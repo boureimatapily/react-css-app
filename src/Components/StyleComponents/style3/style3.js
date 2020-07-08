68c9569dc6d4ff06771e77e1ada858db477e2bd1
unknown <boureimatapily1@gmail.com>
2020-07-08 01:40:02 +0000

    ADD style3

---
+++ src/Components/StyleComponents/style3/style3.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./style3.css"
+
+export default function Style3() {
+    return (
+        <div className="main">
+            <h1 aria-label="Madani"></h1>
+        </div>
+    )
+}
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -4,13 +4,8 @@ import { Container, Grid, Link } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import "../Components/Home/Home.css";
 import ButtonAppBar from "../Components/Home/testCard";
-import Style1 from "../Components/StyleComponents/style1/Style1";
-import Style2 from "../Components/StyleComponents/style2/Style2";
-// import style1 from "../Components/StyleComponents/style3/style3";
-// import style1 from "../Components/StyleComponents/style4/style4";
-// import style1 from "../Components/StyleComponents/style5/style5";
-// import style1 from "../Components/StyleComponents/style6/style6";
-// import style1 from "../Components/StyleComponents/style7/style7";
+import Style3 from "../Components/StyleComponents/style3/Style3";
+
 
 
 const useStyles = makeStyles((theme) => ({
@@ -37,7 +32,7 @@ export default function Home() {
           </div>
         </Grid>
         <Grid item xs={12} md={12} lg={12}>
-          <Style2 />
+          <Style3 />
        
         </Grid>
         
